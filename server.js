67cafda56454c959e12de2ffe0cf4faa16aba230
daryllpenafiel <daryll_penafiel@yahoo.com>
2018-07-08 17:43:47 -0400

    routings working

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,10 +16,8 @@ app.use(bodyParser.json());
 // Routes
 // =============================================================
 
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname + "/app/public/home.html"));
-});
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 
 // =============================================================
 app.listen(PORT, function() {
