7397137648fd0d84fb4f975ab87f5eb6810528ce
daryllpenafiel <daryll_penafiel@yahoo.com>
2018-07-12 21:27:55 -0400

    getting there

--- app/data/friendsData.js
+++ app/data/friendsData.js
@@ -1,19 +1,50 @@
-var friendsArray = [
+var friendsArray = [{
+        name: "Ahmed",
+        photo: "https://marriedbiography.com/wp-content/uploads/2017/03/brock-lesnar.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
     {
-    name:"Ahmed",
-    photo:"https://marriedbiography.com/wp-content/uploads/2017/03/brock-lesnar.jpg",
-    scores:[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
+        "name": "DP",
+        "photo": "http://google.com",
+        "scores": [
+            "2",
+            "2",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Daryll",
+        "photo": "http://google.com",
+        "scores": [
+            "4",
+            "4",
+            "4",
+            "4",
+            "3",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4"
+        ]
     }
 ];
 
